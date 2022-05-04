acac14e1840c7080834f7f9f4c5c9dee1d9b4204
Anton <whafu@kth.se>
2022-05-04 13:57:34 +0200

    Some structuring

--- code/time_series_project.r
+++ code/time_series_project.r
@@ -1,11 +1,12 @@
 library(aTSA)
 library(plotly)
-data <- read.csv("~/KTH/year4/SF2943-Time-Series-Analysis/data/GlobalTemperatures.csv", header=TRUE)[1201:3192, 1:3]
-# library(itsmr)
-print(data)
+library(itsmr)
+
+data_train <- read.csv("../data/GlobalTemperatures.csv", header=TRUE)[1201:2401, 1:2]
+data_test <- read.csv("../data/GlobalTemperatures.csv", header=TRUE)[2401:3001, 1:2]
 plot_ly(
-  x = as.Date(data$"dt"), 
-  y = data$"LandAverageTemperature", 
+  x = as.Date(data_train$"dt"), 
+  y = data_train$"LandAverageTemperature", 
   type = 'scatter', 
   mode = 'lines', 
   name = 'Sample plot')%>%
@@ -24,16 +25,15 @@ plot_ly(
     gridcolor = 'ffff'),
   showlegend = TRUE, width = 1500)
 
-season_comp <- season(data$"LandAverageTemperature", 12)
-trend_comp <- trend(data$"LandAverageTemperature", 1)
-residual_temp <- data$"LandAverageTemperature" - season_comp - trend_comp 
+season_comp <- season(data_train$"LandAverageTemperature", 12)
+trend_comp <- trend(data_train$"LandAverageTemperature", 1)
+residual_temp <- data_train$"LandAverageTemperature" - season_comp - trend_comp 
 
-plot_ly(x = as.Date(data$"dt"), 
+plot_ly(x = as.Date(data_train$"dt"), 
         y = season_comp, 
         type = 'scatter', 
         mode = 'lines', 
         name = 'Sample plot')#%>% 
-
 layout(title = 'Temperature over time',
        plot_bgcolor='#e5ecf6',  
        xaxis = list(  
@@ -48,11 +48,9 @@ layout(title = 'Temperature over time',
          gridcolor = 'ffff'),
        showlegend = TRUE, width = 1500)
 
-residual_temp_test <- data$"LandAverageTemperature" - season_comp
+residual_temp <- data_train$"LandAverageTemperature" - season_comp
 residual_temp <- diff(residual_temp, differences=1)
-residual_temp_test <- diff(residual_temp_test, differences=1)
 adf.test(residual_temp)
 acf(residual_temp)
-acf(residual_temp_test)
 pacf(residual_temp)
-pacf(residual_temp_test)
+
